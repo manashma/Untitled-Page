ba1b5b037149ffcdf1f622c7d7a3893de72b6007
TheHackerOne <86978082+manashma@users.noreply.github.com>
2025-01-29 12:06:48 +0530

    Update connections.php

--- connections.php
+++ connections.php
@@ -1,14 +1,19 @@
 <?php
 $servername = "localhost";
-$username ="root"; // Replace with your MySQL username
+$username = "root"; // Replace with your MySQL username
 $password = ""; // Replace with your MySQL password
 $database = "data"; // Replace with your database name
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $database);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+try {
+    // Create a PDO instance
+    $pdo = new PDO("mysql:host=$servername;dbname=$database;charset=utf8", $username, $password);
+    
+    // Set PDO to throw exceptions for errors
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    // Set PDO to fetch associative arrays by default
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage()); // Stop script and show error message
 }
 ?>
